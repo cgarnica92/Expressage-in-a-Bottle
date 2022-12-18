2e5fa250c8457f59e53649d794f6ef31df944ae3
cgarnica92 <c_garnica92@yahoo.com>
2022-12-18 18:04:15 -0500

    Files Added, Server Set Up

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = requre('./routes/htmlRoutes');
+
+//Initialize app and create port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//Start server on port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
